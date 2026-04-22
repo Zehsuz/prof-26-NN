b4d24fd889e228e0fddfda07ec3070473c20247a
Akita Yamado <akitayamado@gmail.com>
2026-04-22 10:12:07 +0300

    customLogger

--- lib/logger_helper.dart
+++ lib/logger_helper.dart
@@ -1,1 +1,2 @@
-export '';
+export 'src/core/custom_logger.dart';
+export 'src/presentation/widgets/loggable_state.dart';
---
+++ lib/src/presentation/widgets/loggable_state.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:logger_helper/logger_helper.dart';
+
+mixin LoggableWidgetState<T extends StatefulWidget> on State<T>
+    implements CustomLogger {
+  @override
+  void activate() {
+    _log(message: 'activate()');
+    super.activate();
+  }
+
+  @override
+  void initState() {
+    _log(message: 'initState()');
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _log(message: 'dispose()');
+    super.dispose();
+  }
+
+  @override
+  void deactivate() {
+    _log(message: 'deactivate()');
+    super.deactivate();
+  }
+
+  void _log({required String message}) {
+    logInfo(operation: 'Жизненный цикл виджета', message: message);
+  }
+}
