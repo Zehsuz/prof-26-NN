64a8f71a8ab45f321d76f5d3001813138beb8d55
Akita Yamado <akitayamado@gmail.com>
2026-05-27 18:45:26 +0300

    add: LoggingInterceptor

--- lib/net_service.dart
+++ lib/net_service.dart
@@ -2,3 +2,4 @@ export 'src/data/exceptions/exception.dart';
 //
 export 'src/data/utils/error_adapter.dart';
 export 'src/data/utils/auth_interceptor.dart';
+export 'src/data/utils/logging_interceptor.dart';
---
+++ lib/src/data/utils/logging_interceptor.dart
@@ -0,0 +1,47 @@
+import 'package:dio/dio.dart';
+import 'package:logger_helper/logger_helper.dart';
+
+/// Назначение: автоматическое логирование запросов
+/// Дата создания: 26.05.2026
+/// Создал: Захар
+class LoggingInterceptor extends Interceptor with CustomLogger {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    final operation = '${options.method} ${options.uri.pathSegments.join('/')}';
+    logInfo(
+      operation: operation,
+      message: 'Начало выполнения запроса $operation',
+      rtype: 'NetHttpClient',
+    );
+    handler.next(options);
+  }
+
+  @override
+  void onResponse(
+    Response<dynamic> response,
+    ResponseInterceptorHandler handler,
+  ) {
+    final operation =
+        '${response.requestOptions.method} ${response.requestOptions.uri.pathSegments.join('/')}';
+    logInfo(
+      operation: operation,
+      message:
+          'Запрос $operation выполнен успешно! Ответ от сервера: ${response.data}',
+      rtype: 'NetHttpClient',
+    );
+    handler.next(response);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    final operation =
+        '${err.requestOptions.method} ${err.requestOptions.uri.pathSegments.join('/')}';
+    logInfo(
+      operation: operation,
+      message:
+          'NetLibException: возникла ошибка выполнения запроса $operation. Ошибка $err',
+      rtype: 'NetHttpClient',
+    );
+    handler.next(err);
+  }
+}
