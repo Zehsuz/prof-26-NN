ea7f76ada6e4eb81d3c66345775fc8493de0eb8a
Akita Yamado <akitayamado@gmail.com>
2026-05-27 19:16:06 +0300

    add: login

--- test/net_service_test.dart
+++ test/net_service_test.dart
@@ -1,3 +1,30 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:logging/logging.dart';
 
 import 'package:net_service/net_service.dart';
+
+final testString = DateTime.now().microsecondsSinceEpoch;
+final email = '$testString@gmail.com';
+final password = r'Pa$$w0rd';
+
+void main() {
+  Logger.root.level = .ALL;
+  Logger.root.onRecord.listen((log) => print(log.message));
+  late NetHttpClient _client;
+  late AuthInterceptor interceptor;
+  setUp(() {
+    interceptor = AuthInterceptor();
+    _client = NetHttpClient(
+      Dio()..interceptors.addAll([interceptor, LoggingInterceptor()]),
+      baseUrl: 'http://127.0.0.1:8090/api/collections',
+    );
+  });
+
+  test('login', () async {
+    final response = await _client.login(
+      .new(identity: 'qwe@qwe.qwe', password: password),
+    );
+    expect(response, isA<AuthResponse>());
+  });
+}
