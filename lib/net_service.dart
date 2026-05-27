b8630725800dd845d5dbcc2e2da867090d71591e
Akita Yamado <akitayamado@gmail.com>
2026-05-27 19:04:52 +0300

    add: UserModel -10

--- lib/net_service.dart
+++ lib/net_service.dart
@@ -3,3 +3,5 @@ export 'src/data/exceptions/exception.dart';
 export 'src/data/utils/error_adapter.dart';
 export 'src/data/utils/auth_interceptor.dart';
 export 'src/data/utils/logging_interceptor.dart';
+//
+export 'src/data/models/user.dart';
